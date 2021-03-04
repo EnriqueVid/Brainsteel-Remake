b9f9d3a75e3037ad0e0b42b6f659e2cfafa7d5cb
danielo <danieemil@hotmail.com>
2021-03-04 17:42:27 +0100

    Introducción al UI

--- Assets/Sources/Scripts/SpawnFallingSpheres.cs
+++ Assets/Sources/Scripts/SpawnFallingSpheres.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnFallingSpheres : MonoBehaviour
 {
@@ -11,11 +12,13 @@ public class SpawnFallingSpheres : MonoBehaviour
     public float firstSpawnDelay = 5.0f;
     public float spawnInterval = 3.0f;
 
-    IEnumerator corutina;
+    private IEnumerator corutina;
 
     // Spawn location
     public Vector3 spawnPoint;
 
+    // Whether to spawn or not, toggle box
+    public Toggle spawningToggl;
 
     // Start is called before the first frame update
     void Start()
@@ -27,23 +30,15 @@ public class SpawnFallingSpheres : MonoBehaviour
         // When to spawn the falling spheres?
         //InvokeRepeating("SpawnFallingSphere", firstSpawnDelay, spawnInterval);
 
-
         // Coroutine (advanced, please calm down and die silently)
         corutina = CoSpawnFallingSphere();
-        StartCoroutine(corutina);
+        SpawningControl();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    // Spawning a falling sphere function
-    void SpawnFallingSphere()
-    {
-        // Instantiate(GameObject prefab, Vector3 position, Quaternion rotation);
-        Instantiate(fallingSpherePrefab, spawnPoint, fallingSpherePrefab.transform.rotation);
     }
 
     // Coroutines, proceed with caution
@@ -55,4 +50,32 @@ public class SpawnFallingSpheres : MonoBehaviour
             SpawnFallingSphere();
         }
     }
+
+    // Spawning a falling sphere function
+    public void SpawnFallingSphere()
+    {
+        // Instantiate(GameObject prefab, Vector3 position, Quaternion rotation);
+        Instantiate(fallingSpherePrefab, spawnPoint, fallingSpherePrefab.transform.rotation);
+    }
+
+
+
+
+
+
+
+
+    // Called only when the Toggle box has been checked or unchecked
+    public void SpawningControl()
+    {
+        if (spawningToggl.isOn)
+        {
+            StartCoroutine(corutina);
+        }
+        else
+        {
+            StopCoroutine(corutina);
+        }
+    }
+
 }

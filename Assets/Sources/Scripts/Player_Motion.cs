58ae22d2f15632997e912b6876d4de6e305f4011
EnriqueVid <quiquesoyyo@gmail.com>
2021-03-09 17:59:25 +0100

    Character Controller Movement

--- Assets/Sources/Scripts/Player_Motion.cs
+++ Assets/Sources/Scripts/Player_Motion.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class Player_Motion : MonoBehaviour
 {
+    //[Header("References")]                                            -> Añade un titulo en el inspector
+    //[Tooltip("Reference to the main camera used for the player")]     -> Añade una descripcion a la variable declarada debajo cuando haces "Hover" 
+    //[SerializeField]                                                  -> Permite modificar el valor de la variable en el inspector aunque sea PRIVADA
+    //[Range(0.0f, 0.5f)]                                               -> Genera un slider con los rangos establecidos en el inspector para la variable seleccionada
 
+
+    [Header("Camera Options")]
+    [Tooltip("Modifica la velocidad de movimiento de la cámara")]
     public float cameraSensitivity = 100.0f;
-    public float vertRotation = 0;
-    public float velocity = 3.0f;
-    public float gravity = -9.0f;
+    [Tooltip("Indica el ángulo inicial de la cámara")]
+    [SerializeField] [Range(-90.0f, 90.0f)] float vertRotation = 0;
 
+
+    [Header("Movement Options")]
+    [SerializeField] float walkSpeed = 3.0f;
+    [SerializeField] float runSpeed = 5.0f;
+    [SerializeField] float crouchSpeed = 1.5f;
+    [SerializeField] float gravity = -9.0f;
+    [SerializeField] float vSpeed = 0.0f;
+    [SerializeField] bool dJump = false;
+    [SerializeField] float jumpHeight = 2f;
+    [SerializeField] float dJumpHeight = 1f;
+
+
+
+    [SerializeField] bool grounded;
+
+
+
+    private CharacterController controller;
     private Camera playerCamera;
-    private Vector3 vSpeed;
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
+        controller = GetComponent<CharacterController>();
+        controller.enableOverlapRecovery = true;
         playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -24,6 +50,8 @@ public class Player_Motion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        grounded = controller.isGrounded;
+
 
         //ROTATE CAMERA & PLAYER
         float mouseX = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
@@ -38,14 +66,44 @@ public class Player_Motion : MonoBehaviour
 
 
         //MOVE PLAYER
-        vSpeed.y -= gravity * Time.deltaTime;
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        movement *= velocity;
-        movement += vSpeed;
+        
 
+        //Vector2 inputDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));       -> GetAxis() devuelve un valor "smooth" lo que añade delay al movimiento
+        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //-> GetAxisRaw() obtiene el valor "Raw"
+        inputDir.Normalize();
 
+        Vector3 movement = (transform.forward * inputDir.y + transform.right * inputDir.x);
 
-        transform.Translate(movement * Time.deltaTime);
+        movement *= walkSpeed;
+
+        
+
+        if (controller.isGrounded)
+        {
+            vSpeed = -2f;
+            dJump = true;
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (controller.isGrounded)  //Salto normal
+            {
+                vSpeed = Mathf.Sqrt(jumpHeight * 2f * gravity);
+            }
+            else if (dJump) //Doble Salto
+            {
+                dJump = false;
+                vSpeed = Mathf.Sqrt(dJumpHeight * 2f * gravity);
+            }
+        }
+
+        vSpeed -= gravity * Time.deltaTime;
+
+        movement += Vector3.up * vSpeed;
+
+        controller.Move(movement * Time.deltaTime);
+
 
 
 

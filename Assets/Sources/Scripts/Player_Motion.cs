a4c186e99699c933285ce4b4a111c99aada0db9b
EnriqueVid <quiquesoyyo@gmail.com>
2021-03-23 19:35:02 +0100

    Fisicas del Character Controller bastante mejoradas (ya no dan (tanto) asco).

--- Assets/Sources/Scripts/Player_Motion.cs
+++ Assets/Sources/Scripts/Player_Motion.cs
@@ -24,51 +24,67 @@ public class Player_Motion : MonoBehaviour
     [SerializeField] float gravity = -9.0f;
     [SerializeField] float vSpeed = 0.0f;
     [SerializeField] bool dJump = false;
-    [SerializeField] float jumpHeight = 2f;
+    [SerializeField] float jumpHeight = 1.5f;
     [SerializeField] float dJumpHeight = 1f;
 
+    [SerializeField] [Range(0.0f, 60.000000f)] float velocityInterpolation = 15;
+    [Tooltip("[60 FPS], indica cuanto tarda el jugador en cambiar de velocidad | 1/vI ~= tiempo en segundos")]
+
+    Vector3 characterSpeed;
 
 
-    [SerializeField] bool grounded;
 
-    [SerializeField] bool touching;
-    [SerializeField] float angle = 0.0f;
+    bool grounded;
+    Vector3 groundNormal;
+
+    float lastTimeInGround = 0.0f;
+    float lastTimeInGroundMargin = 0.1f;
+
+
+    bool touching;
+    float angle = 0.0f;
+
+
+    [Header("Debug Options")]
+    public GameObject normalPos;
+    public GameObject directorPos;
+    public float t1 = 1.0000f;
+    public float t2 = 1.0000f;
 
 
 
     private CharacterController controller;
     private Camera playerCamera;
 
-    public GameObject spherePos;
+    
     
     
 
     // Start is called before the first frame update
     void Start()
     {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 60;
+
         controller = GetComponent<CharacterController>();
-        controller.enableOverlapRecovery = true;
         playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        characterSpeed = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = controller.isGrounded;
+        GroundCheck();
 
+        Look();
 
-        //ROTATE CAMERA & PLAYER
-        float mouseX = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+        Move();
 
-        transform.Rotate(Vector3.up * mouseX);
+        
 
-        vertRotation -= mouseY;
-        vertRotation = Mathf.Clamp(vertRotation, -85f, 85f);
-        playerCamera.transform.localEulerAngles = Vector3.right * vertRotation;
-
-
+        /*
 
         //MOVE PLAYER
         
@@ -94,6 +110,7 @@ public class Player_Motion : MonoBehaviour
         {
             if (controller.isGrounded)  //Salto normal
             {
+                lastTimeInGround = Time.time;
                 vSpeed = Mathf.Sqrt(jumpHeight * 2f * gravity);
             }
             else if (dJump) //Doble Salto
@@ -117,10 +134,11 @@ public class Player_Motion : MonoBehaviour
 
         if (touching)
         {
-            spherePos.transform.position = hit.point;
+            normalPos.transform.position = hit.point;
 
-            LineRenderer lr = spherePos.GetComponentInChildren<LineRenderer>();
+            LineRenderer lr = normalPos.GetComponentInChildren<LineRenderer>();
             lr.SetPosition(1, hit.normal);
+
 
             //angle = Mathf.Acos(Vector3.Dot(hit.normal, Vector3.up)/ (hit.normal.magnitude * Vector3.up.magnitude));
 
@@ -140,11 +158,140 @@ public class Player_Motion : MonoBehaviour
         //LineRenderer lr2 = GetComponentInChildren<LineRenderer>();
         //lr2.SetPosition(1, movement);
 
+        UpdateDebug(movement);
 
-        
+        */
+    }
+
+    void GroundCheck() 
+    {
+        grounded = false;
+        groundNormal = Vector3.up;
+
+        //Comprobar si ha pasado un tiempo para poder saltar
+        if (Time.time >= lastTimeInGround + lastTimeInGroundMargin)
+        {
+            //Sphere cast para comprobar suelo (0.041 - 0.07)
+            float checkDistance = 0.04f + controller.skinWidth;
+            Vector3 initPoint = transform.position + (Vector3.up * controller.radius);
+
+            if (Physics.SphereCast(initPoint, controller.radius, Vector3.down, out RaycastHit hit, checkDistance, -1, QueryTriggerInteraction.Ignore))
+            {
+                groundNormal = hit.normal;
+
+                if (Vector3.Angle(Vector3.up, hit.normal) <= controller.slopeLimit)
+                {
+                    grounded = true;
+                    dJump = true;
+
+                    // handle snapping to the ground
+                    if (hit.distance > controller.skinWidth)
+                    {
+                        controller.Move(Vector3.down * hit.distance);
+                    }
+
+                }
+            }
+        }
+    }
+
+    void Look() 
+    {
+        //ROTATE CAMERA & PLAYER
+        float mouseX = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        vertRotation -= mouseY;
+        vertRotation = Mathf.Clamp(vertRotation, -85f, 85f);
+        playerCamera.transform.localEulerAngles = Vector3.right * vertRotation;
+    }
 
 
+    void Move() 
+    {
+        //Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //moveDir = Vector3.ClampMagnitude(moveDir, 1);
+        //Vector3 globalMoveDir = transform.TransformVector(moveDir);
 
+
+        Vector3 moveDir = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
+        moveDir = Vector3.ClampMagnitude(moveDir, 1);
+
+        if (grounded)
+        {
+            if (groundNormal != Vector3.up) 
+            {
+                //Reorientar vector del movimiento
+                moveDir = GetReorientedVector(moveDir.normalized) * moveDir.magnitude;
+            }
+
+            moveDir *= walkSpeed;
+                                         
+            characterSpeed = Vector3.Lerp(characterSpeed, moveDir, velocityInterpolation * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                characterSpeed = Vector3.ProjectOnPlane(characterSpeed, Vector3.up);
+                characterSpeed += Vector3.up * Mathf.Sqrt(jumpHeight * 2f * gravity);
+
+                lastTimeInGround = Time.time;
+                grounded = false;
+                groundNormal = Vector3.up;
+            }
+
+        }
+        else 
+        {
+            characterSpeed += moveDir * 25 * Time.deltaTime;
+
+            // limit air speed to a maximum, but only horizontally
+            float verticalVelocity = characterSpeed.y;
+            Vector3 horizontalVelocity = Vector3.ProjectOnPlane(characterSpeed, Vector3.up);
+            horizontalVelocity = Vector3.ClampMagnitude(horizontalVelocity, 10);
+            
+            if (Input.GetButtonDown("Jump") && dJump)
+            {
+                characterSpeed += Vector3.up * Mathf.Sqrt(dJumpHeight * 2f * gravity);
+                dJump = false;
+            }
+            else 
+            {
+                characterSpeed = horizontalVelocity + (Vector3.up * verticalVelocity);
+            }
+
+
+            // apply the gravity to the velocity
+            characterSpeed += Vector3.down * gravity * Time.deltaTime;
+
+        }
+
+
+        controller.Move(characterSpeed * Time.deltaTime);
+
+        Vector3 point1 = transform.position + (Vector3.up * controller.radius);
+        Vector3 point2 = transform.position + (Vector3.up * (controller.height - controller.radius));
+
+        if (Physics.CapsuleCast(point1, point2, controller.radius, characterSpeed.normalized, out RaycastHit hit, characterSpeed.magnitude * Time.deltaTime, -1, QueryTriggerInteraction.Ignore)) 
+        {
+            characterSpeed = Vector3.ProjectOnPlane(characterSpeed, hit.normal);
+        }
+
+    }
+
+    Vector3 GetReorientedVector(Vector3 direction) 
+    {
+        Vector3 resul = Vector3.Cross(direction, Vector3.up);
+        return Vector3.Cross(groundNormal, resul).normalized;
+    }
+
+
+    void UpdateDebug(Vector3 mov) 
+    {
+        Vector3 mov_norm = mov.normalized;
+        directorPos.transform.position = transform.position;
+        directorPos.transform.rotation = Quaternion.LookRotation(mov_norm, Vector3.up);
 
     }
 }
